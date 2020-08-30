0fda624dc4725b8c39d568c6fc616237c0345aa3
parth <parthkanani8490@gmail.com>
2020-08-30 14:45:29 +0530

    initialize two component chat and join and add route fro both

---
+++ src/components/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Chat=()=>{
+    return(
+        <div>
+            <p>Chat</p>
+        </div>
+    )
+}
+
+export default Chat;
---
+++ src/components/Join.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Join=()=>{
+    return(
+        <div>
+            <p>Join</p>
+        </div>
+    )
+}
+
+export default Join;
